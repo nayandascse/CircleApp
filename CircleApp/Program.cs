170993f2a4dbdd92d0adfd9295f60cd98642a878
TAAkhi <sarkar.rajibcse@gamil.com>
2015-05-13 16:51:52 +0600

    CircleApp

---
+++ CircleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle aCircle = new Circle();
+            Console.WriteLine("Enter radius of circle.");
+            aCircle.radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Diameter of the circle is = "+aCircle.GetDiameter());
+            Console.WriteLine("Perimeter of the circle is = "+aCircle.GetPerimeter());
+            Console.WriteLine("Area of the Circle is = "+aCircle.GetArea());
+            Console.ReadKey();
+        }
+    }
+}
